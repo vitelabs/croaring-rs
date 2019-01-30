6a93a2e50a2a64fd7e7e216008ac97f6bad6a54c
shenyang <shenyang@vite.org>
2019-01-30 17:25:50 +0800

    some features for arm64、 armv7s

--- croaring-sys/build.rs
+++ croaring-sys/build.rs
@@ -6,14 +6,14 @@ use std::path::PathBuf;
 
 fn main() {
     cc::Build::new()
-        .flag("-std=c11")
-        .flag("-march=native")
-        .flag("-O3")
+        .flag_if_supported("-std=c11")
+        //.flag_if_supported("-march=native")
+        .flag_if_supported("-O3")
         .file("CRoaring/roaring.c")
         .compile("libroaring.a");
 
     let bindings = bindgen::Builder::default()
-        .blacklist_type("max_align_t")
+        .hide_type("max_align_t")
         .header("CRoaring/roaring.h")
         .generate_inline_functions(true)
         .generate()
